cee9aee354bda7e7d813a3e96b4e334716eed774
DarkBunniee <himanshuhada380@gmail.com>
2023-02-10 07:07:22 -0800

    html-css-js
    an simple website with unsplash api

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// NAVBAR
+const navToggle = document.querySelector(".nav-toggle");
+const navItems = document.querySelector(".nav-items");
+
+navToggle.addEventListener("click", function () {
+  navItems.classList.toggle("show");
+});
